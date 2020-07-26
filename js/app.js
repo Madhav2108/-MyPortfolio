eba463ab2e204adc5a22d841ddc2715ef041a92e
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-07-27 02:14:03 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,73 @@
+/* const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')*/
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  //Remove menu mobile
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));*/
+/*---------------------------------------------------------------------------------------------------------*/
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
+
+const navLink = document.querySelectorAll('.nav-link');
+  function linkAction(){
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    showMenu = false;
+
+  }
+
+  navLink.forEach(n => n.addEventListener('click', linkAction));
