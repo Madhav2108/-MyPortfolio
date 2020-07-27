f8a6b094f3bae900cad60c8ac49f65deef0fea95
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-07-28 02:16:26 +0530

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -26,6 +26,18 @@ navLink.forEach(n => n.addEventListener('click', linkAction));*/
 /*---------------------------------------------------------------------------------------------------------*/
 
 
+
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
@@ -71,3 +83,29 @@ const navLink = document.querySelectorAll('.nav-link');
   }
 
   navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+  document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+    t.style.left = n.clientX + "px", 
+  t.style.top = n.clientY + "px", 
+  e.style.left = n.clientX + "px", 
+  e.style.top = n.clientY + "px", 
+  i.style.left = n.clientX + "px", 
+  i.style.top = n.clientY + "px"
+  });
+  var t = document.getElementById("cursor"),
+    e = document.getElementById("cursor2"),
+    i = document.getElementById("cursor3");
+  function n(t) {
+    e.classList.add("hover"), i.classList.add("hover")
+  }
+  function s(t) {
+    e.classList.remove("hover"), i.classList.remove("hover")
+  }
+  s();
+  for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+    o(r[a])
+  }
+  function o(t) {
+    t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+  }
